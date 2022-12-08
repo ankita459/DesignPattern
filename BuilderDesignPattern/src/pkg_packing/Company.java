e77c6254890606f8e9e13096b2669215b7b0b084
ankita459 <ankitapanpatil18pc@student.mes.ac.in>
2022-12-08 10:26:59 +0530

    Add files via upload

---
+++ BuilderDesignPattern/src/pkg_packing/Company.java
@@ -0,0 +1,5 @@
+package pkg_packing;
+
+public abstract class Company extends CD{  
+	   public abstract int price();  
+	}  
---
+++ BuilderDesignPattern/src/pkg_packing/Packing.java
@@ -0,0 +1,6 @@
+package pkg_packing;
+
+public interface Packing {  
+    public String pack();  
+    public int price();  
+}  
